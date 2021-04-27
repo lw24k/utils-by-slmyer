3d80461a2300ff9e4840bed2f01bc13f61bd0d8f
slmyer <15156272530@163.com>
2021-04-27 21:07:51 +0800

    fix

--- utils/js-helper/instance.js
+++ utils/js-helper/instance.js
@@ -1,24 +1,40 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: slmyer
+ * @Date: 2021-04-26 21:03:06
+ * @LastEditTime: 2021-04-27 21:07:17
+ */
 const Instance = (target, source) => {
-  if (!source || !target) {
-    throw "error";
+  try {
+    const _prototype = source.prototype;
+    return _prototype.isPrototypeOf(target);
+  } catch (e) {
+    throw new Error(e);
   }
-  const _prototype = source.prototype;
-  return _prototype.isPrototypeOf(target);
 };
 
 const InstanceBy = (target, source) => {
-  if (!source || !target) {
-    throw "error";
-  }
-  let _p = Object.getPrototypeOf(target);
-
-  while (true) {
-    if (_p === null) {
+  try {
+    if (target === null) {
       return false;
     }
-    if (_p === source.prototype) {
-      return true;
+    const baseType = ["string", "number", "boolean", "undefined", "symbol"];
+    if (baseType.includes(typeof target)) {
+      return false;
     }
-    _p = Object.getPrototypeOf(_p);
+    let _p = Object.getPrototypeOf(target);
+
+    while (true) {
+      if (_p === null) {
+        return false;
+      }
+      if (_p === source.prototype) {
+        return true;
+      }
+      _p = Object.getPrototypeOf(_p);
+    }
+  } catch (e) {
+    throw new Error(e);
   }
 };
