5c706d857b08a3fb5a2c2aba3059f5b9e05d1c40
slmyer <15156272530@163.com>
2021-04-27 21:59:10 +0800

    bind

---
+++ utils/js-helper/bind-helper.js
@@ -0,0 +1,27 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: slmyer
+ * @Date: 2021-04-27 21:08:43
+ * @LastEditTime: 2021-04-27 21:57:46
+ */
+
+/**
+ * @description:
+ * bind  改变this指向问题以及不会影响原函数返回
+ * 当bind后的函数 使用new 进行构造时 之前的bind this会丢失 但是传入的参数不会丢失
+ * 同时当我们对bind 之后的函数进行原型对象操作不会影响到本身的函数
+ * @param {*}
+ * @return {*}
+ */
+Function.prototype.myBind = function () {
+  const self = this;
+  const context = Array.prototype.shift.call(arguments);
+  const args = Array.prototype.slice.call(arguments);
+  let _bindFunc = function () {
+    const _args = Array.prototype.slice.call(arguments);
+    return self.apply(context, args.concat(_args));
+  };
+  _bindFunc.prototype = Object.create(this.prototype);
+  return _bindFunc;
+};
