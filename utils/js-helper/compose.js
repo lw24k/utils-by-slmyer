ada1c243b74842a1ed1c9f1c480eba4ca64e0ffb
slmyer <15156272530@163.com>
2021-05-07 22:47:48 +0800

    feat -  新增compose

---
+++ utils/js-helper/compose.js
@@ -0,0 +1,28 @@
+/*
+ * @Descripttion:
+ * @version:  compose 函数 后一个参数是前一个参数的入参 redux applymiddleware
+ * @Author: slmyer
+ * @Date: 2021-05-07 22:29:02
+ * @LastEditTime: 2021-05-07 22:46:22
+ */
+
+const compose = (...fns) => {
+  if (fns.length === 0) {
+    return (arg) => arg;
+  }
+  if (fns.length === 1) {
+    return fns[0];
+  }
+  return fns.reduce((a, c) => (...args) => a(c(...args)));
+};
+
+const _compose = (...fns) => {
+  let index = fns.length - 1;
+  return (...args) => {
+    let result = fns.length > 0 ? fns[index].apply(this, args) : args;
+    while (index-- > 0) {
+      result = fns[index].call(this, result);
+    }
+    return result;
+  };
+};
